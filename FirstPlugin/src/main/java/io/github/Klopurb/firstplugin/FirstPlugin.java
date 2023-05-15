46dc6fbec2b04a64258166eb15c301418e6ab924
Klopurb <sebastianbrownh@yahoo.com>
2023-05-14 22:06:22 -0500

    Update FirstPlugin.java

--- FirstPlugin/src/main/java/io/github/Klopurb/firstplugin/FirstPlugin.java
+++ FirstPlugin/src/main/java/io/github/Klopurb/firstplugin/FirstPlugin.java
@@ -2,6 +2,15 @@ package io.github.Klopurb.firstplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FirstPlugin extends JavaPlugin {
+public final class FirstPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
 
 }
